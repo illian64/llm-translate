e9e0e647f7422c98fc3cfef51c379acc3c34fe25
APodoinikov <APodoynikov@detmir.ru>
2025-07-27 22:16:27 +0700

    skip numeric-only parts

---
+++ test/test_struct.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from app.struct import Part
+
+
+class StructTest(TestCase):
+    def test_is_numeric_or_empty(self):
+        self.assertEqual(False, Part("1 000 000 c", False).is_numeric_or_empty())
+
+        self.assertEqual(True, Part("1.23", False).is_numeric_or_empty())
+        self.assertEqual(True, Part("1,23", False).is_numeric_or_empty())
+        self.assertEqual(True, Part("  ", False).is_numeric_or_empty())
+        self.assertEqual(True, Part("1 000 000", False).is_numeric_or_empty())
+        self.assertEqual(True, Part("Ⅻ", False).is_numeric_or_empty())
+        self.assertEqual(True, Part("...", False).is_numeric_or_empty())
