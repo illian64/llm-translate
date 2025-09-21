8b95486447b1b2b1469d5a901032dbc596294c17
illian64 <illian@mail.ru>
2025-09-21 16:05:43 +0700

    Fix test for github actions
    Co-authored-by: APodoinikov <APodoynikov@detmir.ru>

--- tests/test_file_processor.py
+++ tests/test_file_processor.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 from unittest import TestCase
 
 from app import file_processor
 
 
 class FileProcessTest(TestCase):
+    test_dir = Path(__file__).parent.absolute()
+
     def test_encoding(self):
-        content = file_processor.read_file_with_fix_encoding("../files/test_encoding_ansi.txt")
+        content = file_processor.read_file_with_fix_encoding(str(self.test_dir / "files/test_encoding_ansi.txt"))
         self.assertEqual("Hello, World. Привет, Мир. Ёё.", content)
