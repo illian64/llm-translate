155e1d51a69a8ed926a38a54b180ff597126a3c3
APodoinikov <APodoynikov@detmir.ru>
2025-10-18 19:26:09 +0700

    special prompt for model

--- plugins/plugin_kobold_cpp.py
+++ plugins/plugin_kobold_cpp.py
@@ -10,6 +10,7 @@ from app.lang_dict import get_lang_by_2_chars_code
 
 plugin_name = os.path.basename(__file__)[:-3]  # calculating modname
 logger = log.logger()
+model_name: str = ""
 
 
 def start(core: AppCore):
@@ -22,6 +23,9 @@ def start(core: AppCore):
             "prompt": "You are a professional translator. Your task is to translate a text provided below from %%from_lang%% to %%to_lang%%.\n%%context_prompt%%\nINSTRUCTION:Carefully analyze the context. Pay special attention to Terminology, Style, Consistency. Provide only the translation. Do not include any additional information, explanations, notes, or comments in your response. The output should be the pure translated text only.\nTEXT TO TRANSLATE:",
             "prompt_postfix": "",
             "prompt_no_think_postfix": False,
+            "special_prompt_for_model": {
+                "my_model_name": "special prompt"
+            },
         },
         "translate": {
             "kobold_cpp": (init, translate)  # 1 function - init, 2 - translate
@@ -41,8 +45,10 @@ def init(core: AppCore) -> TranslatePluginInitInfo:
 
     url_model = options['custom_url'] + "/api/v1/model"
     response_model = get_json_resp(url_model)
+    global model_name
+    model_name = response_model["result"]
 
-    return TranslatePluginInitInfo(plugin_name=plugin_name, model_name=response_model["result"])
+    return TranslatePluginInitInfo(plugin_name=plugin_name, model_name=model_name)
 
 
 def translate(core: AppCore, ts: TranslateStruct):
@@ -51,7 +57,10 @@ def translate(core: AppCore, ts: TranslateStruct):
     from_lang_name: str = get_lang_by_2_chars_code(ts.req.from_lang)
     to_lang_name: str = get_lang_by_2_chars_code(ts.req.to_lang)
 
-    prompt = translate_func.generate_prompt(prompt_param=options["prompt"], from_lang_name=from_lang_name,
+    special_prompt_for_model: str | None = options["special_prompt_for_model"].get(model_name)
+    prompt_param = special_prompt_for_model if special_prompt_for_model else options["prompt"]
+
+    prompt = translate_func.generate_prompt(prompt_param=prompt_param, from_lang_name=from_lang_name,
                                             to_lang_name=to_lang_name, postfix_param=options["prompt_postfix"],
                                             prompt_no_think_postfix_param=options['prompt_no_think_postfix'],
                                             context=ts.req.context)
